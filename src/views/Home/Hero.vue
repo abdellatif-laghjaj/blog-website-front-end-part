caf06a4b10dd3e4fac0cbba666d42953816cbd96
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-09-25 15:18:17 +0100

    hero

--- src/views/Home/Hero.vue
+++ src/views/Home/Hero.vue
@@ -2,12 +2,14 @@
   <div class="hero min-h-screen">
     <img src="/images/hero.jpg" alt="hero image" class="block w-full h-full object-cover">
     <div class="hero-overlay bg-opacity-80"></div>
-    <div class="hero-content text-center text-white">
+    <div class="hero-content text-center text-white sm:mt-[-10rem]">
       <div class="max-w-md">
-        <h1 class="mb-5 text-5xl font-bold">Hello there</h1>
-        <p class="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-          quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-        <button class="btn btn-primary">Get Started</button>
+        <h1 class="mb-1 text-4xl font-bold">Welcome to</h1>
+        <h1 class="mb-5 text-4xl font-bold">Abdel. <span class="text-primary">Blog</span></h1>
+        <p class="mb-5">A modern blog website talk about technology, programming, and more.</p>
+        <p class="mb-5">This blog is built with Laravel and Vue.js.</p>
+        <router-link to="/articles" class="btn btn-primary mx-1">Read Articles</router-link>
+        <router-link to="/contact" class="btn btn-secondary mx-1">Contact us</router-link>
       </div>
     </div>
   </div>
