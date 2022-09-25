75e3e31c402b6842a8b22dfd4eb3de45ef890b5f
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-09-25 01:00:03 +0100

    add fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+    mode: 'jit',
     content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
     theme: {
         extend: {},
