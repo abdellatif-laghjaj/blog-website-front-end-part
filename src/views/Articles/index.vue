a259d543395260cce28af830fc85b3a8017504c2
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-09-25 22:40:18 +0100

    Finish articles page design

--- src/views/Articles/index.vue
+++ src/views/Articles/index.vue
@@ -2,13 +2,22 @@
   <InnerPageHero title="Our" subtitle="Articles" image-url="/images/articles.jpg"/>
 
   <div class="mx-auto py-8 px-4 sm:py-12 sm:px-8">
-    <Articles :shwo-view-more="false"/>
+    <Articles :show-view-more="false"/>
+
+    <!-- Show more -->
+    <div class="flex justify-center mt-4">
+      <button class="btn gap-2 flex items-center gap-2">
+        <span>Load More</span>
+        <box-icon name='refresh' color="#fff"></box-icon>
+      </button>
+    </div>
   </div>
 </template>
 
 <script>
 import InnerPageHero from "@/components/InnerPageHero";
 import Articles from "@/components/Articles";
+import 'boxicons'
 
 export default {
   name: "index",
